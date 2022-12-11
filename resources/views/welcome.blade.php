3cb31859cf5bd8fe72bd342dd7598c317bb7e73f
Piccolo <kabiryusufbashir@gmail.com>
2022-12-11 18:53:37 +0100

    Create App setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Verify Ladder</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        Verify Ladder
+    </body>
+</html>
